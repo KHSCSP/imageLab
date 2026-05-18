bfcd057864df007533ecd0b7500b59e511dc1bdd
heyscottn <heyscottn@gmail.com>
2026-05-18 14:54:02 -0500

    mm

--- explore.py
+++ explore.py
@@ -3,10 +3,10 @@ import my_functions as mf
 
 
 print("\n\nhere's some info about the picture:")
-mf.experiment('u15imagefilters\pic.jpg', 'u15imagefilters\mod.jpg')
+mf.experiment('imageLab\pic.jpg', 'imageLab\mod.jpg')
 
 
-# mf.block("u15imagefilters/pic.jpg", "u15imagefilters/pic_block.jpg")
+# mf.block("imageLab/pic.jpg", "imageLab/pic_block.jpg")
 # print("what did this do?")
 
 
